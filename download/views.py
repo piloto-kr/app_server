b1be29dcb26ed3d23541ae61068ce11e1212650f
Loki <loki.piloto@gmail.com>
2021-06-22 12:51:19 +0000

    Fix download file name and path

--- download/views.py
+++ download/views.py
@@ -3,14 +3,14 @@ from django.http import HttpResponse
 
 
 def download_file(request):
-    filepath = '/home/piloto/git/django_server/files/piloto0618.apk'
+    filepath = '/home/piloto/git/app_server/files/piloto.apk'
     response = HttpResponse()
     response['X-File'] = filepath
     response['Content-Disposition'] = 'attachment; filename=piloto.apk'
     return response
 
 def download_file_response(request):
-    response = FileResponse(open('files/piloto0618.apk', 'rb'),
+    response = FileResponse(open('files/piloto.apk', 'rb'),
                             content_type='application/force-download')
     return response
 
